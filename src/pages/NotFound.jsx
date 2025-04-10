33f5e99250cd06d1472b168c87383eb98c5d6c30
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-11 09:46:15 +1000

    Modify NotFoundPage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import AppButton from "../components/common/AppButton";
+import { Container } from "react-bootstrap";
+
 function NotFound() {
-	return <div>Hmmmm nothing here</div>;
+	const navigate = useNavigate();
+
+	function handleClick() {
+		navigate("/home");
+	}
+	return (
+		<Container>
+			<h2>Sorry!</h2>
+			<p>The page you were looking for is not available</p>
+			<AppButton onClick={handleClick}>Home</AppButton>
+		</Container>
+	);
 }
 
 export default NotFound;
