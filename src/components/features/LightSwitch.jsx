adb21972a4208e6e833adba53e998b5a76f20060
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-11 11:09:38 +1000

    Add comments

--- src/components/features/LightSwitch.jsx
+++ src/components/features/LightSwitch.jsx
@@ -1,33 +1,66 @@
+// LightSwitch Component
+
+// This component provides a button that allows the user to toggle between dark and light mode for the application. It tracks the times when dark mode is activated and stores these times in local storage. It also adjusts the dark mode based on the average activation time recorded in the local storage.
+
+// State Management:
+// - `darkMode`: State used to track if dark mode is on, allowing to toggle between dark and light mode.
+
+// Methods:
+// - `toggleDarkMode`
+// - `saveTime`
+
+// React Hooks
 import { useEffect, useState } from "react";
+
+// Components
 import AppButton from "../common/AppButton";
 
+// LightSwitch Functional Component
+
+// Toggles the application's dark mode on or off. The component also tracks the times when dark mode is activated and uses those times to calculate whether dark mode should be activated automatically on page load.
 function LightSwitch() {
 	const [, setDarkMode] = useState(false);
 
+	// toggles between dark and light mode by changing the body's class name and saving the current time to local storage.
 	function toggleDarkMode() {
+		// toggle the current dark mode status
 		setDarkMode((prevMode) => {
 			const nextMode = !prevMode;
 			document.body.className = nextMode ? "dark-mode" : "";
+			// save the current hour
 			saveTime();
 			return nextMode;
 		});
 	}
 
+	// saves the current hour to local storage when dark mode is toggled.
 	function saveTime() {
+		//  get current date and time
 		const now = new Date();
+
+		// retrieve array of times stored
 		const darkModeTimes = JSON.parse(
 			localStorage.getItem("darkModeTimes") || "[]"
 		);
+
+		// add current hour
 		darkModeTimes.push(now.getHours());
 
+		// save to localStorage
 		localStorage.setItem("darkModeTimes", JSON.stringify(darkModeTimes));
 	}
 
+	// on component mount, checks recorded dark mode activation times in local storage and activates dark mode automatically if current time is greater or equal to average dark mode activation times
 	useEffect(() => {
+		// retrieve array of times stored
 		const darkModeTimes = JSON.parse(
 			localStorage.getItem("darkModeTimes") || "[]"
 		);
+
+		// initialise variable to hold average time
 		let averageTime = 0;
+
+		// if there are any recorded dark mode activation times, calculate average activation time
 		if (darkModeTimes.length > 0) {
 			const averageTotal = darkModeTimes.reduce((previous, current) => {
 				return previous + current;
@@ -35,14 +68,15 @@ function LightSwitch() {
 			averageTime = averageTotal / darkModeTimes.length;
 		}
 
+		// get current time
 		const now = new Date();
+
+		// if the current hour is greater than or equal to the average time, enable dark mode
 
 		if (now.getHours() >= averageTime) {
 			setDarkMode(true);
 			document.body.className = "dark-mode";
 		}
-
-		console.log("checkingdarkmode");
 	}, []);
 
 	return (
--- src/components/features/Show/ShowListItem.jsx
+++ src/components/features/Show/ShowListItem.jsx
@@ -1,34 +1,77 @@
+// ShowListItem Component
+
+// This component represents a single item in a list of shows (ShowList Component). It displays the show LuPoundSterling, title and provides functionality for interacting with the show (viewing details and removing the item from a list)
+
+// Contexts used:
+// - 'CurrentShowContext': Provides the ability to set the active show and pass show details to other components.
+// - 'CardContext': Manages visibility of the remove button (X)
+
+// Props:
+// 'showInfo'
+// 'setListArray'
+
+// Methods:
+// -  `handleOpenShowDetail`
+// `handleRemove`
+
+// React Hooks
 import { useContext } from "react";
-import { CurrentShowContext } from "../../contexts/CurrentShowContext";
-import { Card, Col, Row } from "react-bootstrap";
-import AppButton from "../../common/AppButton";
-import { CardContext } from "../../contexts/CardContext";
-import { updateLocalStorage } from "../../../UtilityFunctions";
 import { useParams } from "react-router-dom";
 
+// Bootstrap Components
+import { Card } from "react-bootstrap";
+
+// Contexts
+import { CurrentShowContext } from "../../contexts/CurrentShowContext";
+import { CardContext } from "../../contexts/CardContext";
+
+// Components
+import AppButton from "../../common/AppButton";
+
+// Functions
+import { updateLocalStorage } from "../../../UtilityFunctions";
+
+// ShowListItem Functional Component
+
+// Displays a show item within a list. The component allows interaction via clicks on the card to view show details, and it also includes a remove button to remove the show from the list.
+
+// @param {Object} showInfo - An object containing show data
+// @param {Function} setListArray - A function to update the list of shows after an item is removed
 function ShowListItem({ showInfo, setListArray }) {
+	// extract 'id' from URL
 	const { id } = useParams();
 
+	// access CurrentShowContext
 	const currentShowSetting = useContext(CurrentShowContext);
 
+	// opens the detailed view of the show when the card (but not the remove button) is clicked.
+	// @param {Event} e - The event triggered by the card click
 	function handleOpenShowDetail(e) {
+		// Check if the target is not the remove button
 		if (e.target.tagName !== "BUTTON") {
+			// Set the show as active and pass the show data to the context
 			currentShowSetting.setShowActive(true);
 			currentShowSetting.setCurrentShow(showInfo);
 		}
 	}
 
+	// access CurrentShowContext
+	// controls whether "X" remove button is displayed.
 	const { cardRemoveButton } = useContext(CardContext);
 
+	// handles the removal of the show from the list, updating localStorage and the show list context.
 	function handleRemove() {
 		updateLocalStorage(id, showInfo, setListArray, id);
 	}
 
 	return (
 		<Card className="show-item" onClick={handleOpenShowDetail}>
+			{/* Conditionally render the remove button if cardRemoval is true */}
 			{cardRemoveButton && (
 				<AppButton onClick={handleRemove}>X</AppButton>
 			)}
+
+			{/* Render the show poster if available */}
 			{showInfo.image && (
 				<Card.Img
 					variant="top"
@@ -37,7 +80,9 @@ function ShowListItem({ showInfo, setListArray }) {
 					className="poster img-fluid"
 				/>
 			)}
+
 			<Card.Body className="d-flex align-items-center justify-content-center h-100">
+				{/* Display the show name as a title */}
 				<Card.Title className="text-center w-100">
 					{showInfo.name}
 				</Card.Title>
